fccd05c4354ab13080ae6323e87cf182d10e5956
Kritika Singh <kritika1296@gmail.com>
2019-10-10 13:07:33 +0530

    SignIn/SignOut tested

--- routes/bands.js
+++ routes/bands.js
@@ -21,4 +21,70 @@ route.post('/create', (req, res) => {
     })
 })
 
+route.patch('/edit/:bandId', (req, res) => {
+        const updateOps = {};
+        updateOps["name"] = req.body.name;
+        Band.update(updateOps, { where: { id: req.params.bandId } })
+            .then(result => {
+                res.status(200).json({
+                    message: "band updated",
+                    request: {
+                        type: "GET",
+                        url: "http://localhost:3000/bands/"
+                    }
+                });
+    
+            })
+            .catch(err => {
+                res.status(500).json({ error: "some error occured" });
+            });
+}) 
+
+route.get('/', (req, res) => {
+        if (req.session.user) {
+            Band.findAll({
+                where: {
+                    userId: parseInt(req.session.user.id)
+                }
+            })
+                .then((bands) => {
+                    res.status(200).send(bands);
+                })
+                .catch((err) => {
+                    console.log(err);
+                    req.session.user = null;
+                    res.status(500).send({
+                        error: "some error occured"
+                    })
+                });
+        }
+        else {
+            res.status(500).send({
+                error: "User not logged in"
+            })
+        }
+}) 
+
+route.delete('/delete', (req, res) => {
+        if (req.session.user) {
+            Band.destroy({
+                where: {
+                    userId: req.session.user.id,
+                    name:req.body.name,
+                }
+            })
+                .then((band) => {
+                    res.send("Removed Successfully");
+                })
+                .catch((err) => {
+                    res.send("Some Error Occured!!!")
+                })
+        }
+        else {
+            res.status(501).send({
+                error: "User Not Logged In"
+            })
+        }
+}) 
+
 exports = module.exports = route 
